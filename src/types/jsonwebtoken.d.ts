1242fe48712c5bab7fc3bc642a6206b92661655b
PriyaGhimiray18 <02230143.cst@rub.edu.bt>
2025-05-22 12:17:53 +0600

    Fix: problem

--- src/types/jsonwebtoken.d.ts
+++ src/types/jsonwebtoken.d.ts
@@ -1,1 +1,9 @@
-declare module "jsonwebtoken";
+// src/@types/jsonwebtoken.d.ts
+declare module 'jsonwebtoken' {
+  export interface JwtPayload {
+    [key: string]: any;
+  }
+
+  export function sign(payload: string | object | Buffer, secretOrPrivateKey: string, options?: object): string;
+  export function verify(token: string, secretOrPublicKey: string, options?: object): JwtPayload | string;
+}
